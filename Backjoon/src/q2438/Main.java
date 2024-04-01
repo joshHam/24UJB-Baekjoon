cf5bcbcfa903986d8df08df168d6af99b006724b
21-manic-011g <21-manic-011g@21-manic-011>
2024-04-01 17:30:15 +0900

    n

--- Backjoon/src/q2438/Main.java
+++ Backjoon/src/q2438/Main.java
@@ -6,14 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int inp = sc.nextInt();
+		int N = sc.nextInt();
 		sc.close();
 		
-		
-		for(int i=1; i<=inp; i++) {			
+		//첫번째for문 행의개수
+		for(int i=1; i<=N; i++) {			
+		//두번째for문 열의개수
 			for(int j=1; j<=i; j++) {
 			System.out.print("*");
 			}
+			//줄바꿈을 위해
 			System.out.println(" ");
 		}
 	}
