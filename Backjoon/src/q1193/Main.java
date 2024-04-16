3904e523ae7a5900c1b98de48cc1f6d96814e631
21-manic-011g <21-manic-011g@21-manic-011>
2024-04-16 17:48:41 +0900

    1193

---
+++ Backjoon/src/q1193/Main.java
@@ -0,0 +1,42 @@
+package q1193;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		sc.close();
+
+		int s = 0;//규칙숫자간 간격
+		int k = 0;
+		for (int i = 1; i < X; i++) {
+			s += i;
+			if (X > s) {
+				k = i;
+			}
+		}
+
+//		System.out.print(k);
+
+		int j = 0;
+		for (int i = 1; i <= k; i++) {
+			j += i;
+		}
+
+		//좌우값
+		int A = X - j;
+		int B = (k + 2) - (X - j);
+
+		if (k % 2 != 0) {
+			System.out.print(A + "/");
+			System.out.print(B);
+		} else {
+			System.out.print(B + "/");
+			System.out.print(A);
+		}
+
+	}
+
+}
