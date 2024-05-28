fa589ee966f9e20b65c41ea856ea42a2900103eb
joshHam <163796630+joshHam@users.noreply.github.com>
2024-05-28 17:46:29 +0900

    d

---
+++ Backjoon/src/q1110/Main.java
@@ -0,0 +1,21 @@
+package q1110;
+
+import java.util.Scanner;		//안상환풀이 강사풀이랑 같음
+public class Main {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int input = sc.nextInt();
+      sc.close();
+      int count = 0;
+      int B = input;
+      while(true) {
+         B= (10*(B%10))+(((B/10)+(B%10))%10);
+         count++;
+         
+         if(B == input) {
+            System.out.println(count);
+            break;
+         }
+      }
+   }
+}
---
+++ Backjoon/src/q1149/Main.java
@@ -0,0 +1,27 @@
+package q1149;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+//		int[][] price =;
+		
+		
+		
+		for(int i=0;i<N;i++) {
+			int r = sc.nextInt();
+			int g = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(r +" "+ g +" "+ b);	
+		}
+		sc.close();
+		
+	}
+
+}
